2d10305fabf3aa80bc84a7ff25836f83fbe1d2e7
Omar García López <omar.garcia@omargl.net>
2024-05-15 21:52:20 -0600

    add SerieService

---
+++ backend/src/main/java/com/aluracursos/screenmatch/service/SerieService.java
@@ -0,0 +1,28 @@
+package com.aluracursos.screenmatch.service;
+
+import com.aluracursos.screenmatch.dto.SerieDTO;
+import com.aluracursos.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SerieService {
+    @Autowired
+    private SerieRepository repository;
+
+    public List<SerieDTO> obtenerTodasLasSeries(){
+        return repository.findAll().stream()
+                .map(s -> new SerieDTO(
+                        s.getTitulo(),
+                        s.getTotalTemporadas(),
+                        s.getEvaluacion(),
+                        s.getPoster(),
+                        s.getGenero(),
+                        s.getActores(),
+                        s.getSinopsis()))
+                .collect(Collectors.toList());
+    }
+}
