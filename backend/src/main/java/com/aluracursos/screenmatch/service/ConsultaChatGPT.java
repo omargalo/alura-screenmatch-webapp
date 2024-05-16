4dac80c6a74dde8e582ce10094899dbdf919293b
Omar García López <omar.garcia@omargl.net>
2024-05-15 18:56:49 -0600

    first commit

---
+++ backend/src/main/java/com/aluracursos/screenmatch/service/ConsultaChatGPT.java
@@ -0,0 +1,20 @@
+package com.aluracursos.screenmatch.service;
+
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.service.OpenAiService;
+
+public class ConsultaChatGPT {
+    public static String obtenerTraduccion(String texto) {
+        OpenAiService service = new OpenAiService(System.getenv("OPENAI_APIKEY"));
+
+        CompletionRequest requisicion = CompletionRequest.builder()
+                .model("gpt-3.5-turbo-instruct")
+                .prompt("Traduce al español de México en tono chilango el siguiente texto: " + texto)
+                .maxTokens(1000)
+                .temperature(0.7)
+                .build();
+
+        var respuesta = service.createCompletion(requisicion);
+        return respuesta.getChoices().get(0).getText();
+    }
+}
